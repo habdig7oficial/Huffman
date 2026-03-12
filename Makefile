430a41d90d4d772133ce64937d2e39c8d8e0aed0
Mateus Felipe da Silveira Vieira <habdig7@gmail.com>
2026-03-12 19:22:52 -0300

    zweite commit

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
+ARGS ?=
+
 compile:
 	javac -d classes/ src/*.java
 
 run: compile
-	cd classes && java Program
+	cd classes && java Program $(ARGS)
--- src/Program.java
+++ src/Program.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.print("Willkommen!");
+        String files[] = new String[1];
+
+        if(args.length == 0){
+            System.out.println("Geben Sie eine Datei ein\t/\tInput a file\t/tEscolha um arquivo");
+            Scanner input = new Scanner(System.in);
+            files[0] = input.nextLine();
+        }
+        else {
+            files = new String[args.length];
+            for (int i = 0; i < args.length; i++) {
+                files[i] = args[i];
+            }
+        }
+
+        for(String file : files){
+            System.out.println(file);
+        }
+
     }
 }
